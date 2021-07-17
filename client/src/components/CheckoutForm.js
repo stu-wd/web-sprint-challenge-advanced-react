e7bc6cf1b649293dad5e654f8fc6d6157313182a
Stuart Darsey <stuart.darsey@gmail.com>
2021-07-17 18:49:00 -0400

    mvp

--- client/src/components/CheckoutForm.js
+++ client/src/components/CheckoutForm.js
@@ -15,12 +15,18 @@ const initialValue = {
 // and replace the necessary stateful logic from CheckoutForm with the hook
 
 const CheckoutForm = (props) => {
-   //const [showSuccessMessage, setShowSuccessMessage] = useState(false);
-  const [values, handleChanges, handleSubmit, showSuccessMessage] = useForm(
-    'plantValues',
-    initialValue
-  )
+  const [showSuccessMessage, setShowSuccessMessage] = useState(false);
+  // const [values, setValues] = useState(initialValue);
+  const [values, handleChanges] = useForm(initialValue);
 
+  // const handleChanges = (e) => {
+  //   setValues({ ...values, [e.target.name]: e.target.value });
+  // };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setShowSuccessMessage(true);
+  };
 
   // const handleChanges = (e) => {
   //   setValues({ ...values, [e.target.name]: e.target.value });
